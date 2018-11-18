75d065ff0fdc2dd84ae2b0b01e1364e551465a29
liuhua <3158459@qq.com>
2018-11-18 21:27:42 +0800

    1

--- blyx-h5/WebContent/distributor/js/di/star.common.js
+++ blyx-h5/WebContent/distributor/js/di/star.common.js
@@ -35,17 +35,27 @@ function ajax(options){
 }
 
 function getLoginInfo(){
-  var tem = cookieStorage.getItem("login_distributor_user");
-  if(!tem) return null;
-  var user = JSON.parse(tem);
-  return user;
+  try{
+    var tem = cookieStorage.getItem("login_distributor_user");
+    if(!tem) return null;
+    var user = JSON.parse(tem);
+    return user;
+  }catch(e){
+    cookieStorage.removeItem('login_distributor_user');
+    return null;
+  }
 }
 
 function islogin(){
-  var tem = cookieStorage.getItem("login_distributor_user");
-  if(!tem) return false;
-  var user = JSON.parse(tem);
-  return user.distributorId > 0;
+  try{
+    var tem = cookieStorage.getItem("login_distributor_user");
+    if(!tem) return false;
+    var user = JSON.parse(tem);
+    return user.distributorId > 0;
+  }catch(e){
+    cookieStorage.removeItem('login_distributor_user');
+    return false;
+  }
 }
 
 if(! islogin()){
@@ -84,16 +94,16 @@ function logout(){
 }
 
 function putLocalData(key, value){
-  localStorage[key] = value;
+  starStorage.setItem(key, value);
 }
 
 function getLocalData(key){
-  return localStorage[key];
+  return starStorage.getItem(key);
 }
 
 function delLocalData(key){
-  var val = localStorage[key];
-  localStorage.removeItem(key);
+  var val = starStorage.getItem(key);
+  starStorage.removeItem(key);
   return val;
 }
 
@@ -165,3 +175,111 @@ Date.prototype.format = function (fmt) {
   if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
   return fmt;
  }
+window.cookieStorage = (new (function(){
+  var maxage = 60*60*24*1000;
+  var path = '/';
+
+  var cookie = getCookie();
+
+  function getCookie(){
+      var cookie = {};
+      var all = document.cookie;
+      if(all === "")
+          return cookie;
+      var list = all.split("; ");
+      for(var i=0; i < list.length; i++){
+          var cookies = list[i];
+          var p = cookies.indexOf("=");
+          var name = cookies.substring(0,p);
+          var value = cookies.substring(p+1);
+          value = decodeURIComponent(value);
+          cookie[name] = value;
+      }
+      return cookie;
+  }
+
+  var keys = [];
+  for(var key in cookie)
+      keys.push(key);
+
+  this.length = keys.length;
+
+  this.key = function(n){
+      if(n<0 || n >= keys.length)
+          return null;
+      return keys[n];
+  };
+
+  this.setItem = function(key, value){
+      if(! (key in cookie)){
+          keys.push(key);
+          this.length++;
+      }
+
+      cookie[key] = value;
+      var cookies = key + "=" +encodeURIComponent(value);
+      if(maxage)
+          cookies += "; max-age=" + maxage;
+      if(path)
+          cookies += "; path=" + path;
+      cookies += "; domain=hnkbmd.com";
+      document.cookie = cookies;
+  };
+
+  this.getItem = function(name){
+      return cookie[name] || null;
+  };
+
+  this.removeItem = function(key){
+      if(!(key in cookie))
+          return;
+
+      delete cookie[key];
+
+      for(var i=0; i<keys.length; i++){
+          if(keys[i] === key){
+              keys.splice(i, 1);
+              break;
+          }
+      }
+      this.length--;
+
+      document.cookie = key + "=; max-age=0; domain=hnkbmd.com";
+  };
+
+  this.clear = function(){
+      for(var i=0; i<keys.length; i++)
+          document.cookie = keys[i] + "; max-age=0; domain=hnkbmd.com";
+      cookie = {};
+      keys = [];
+      this.length = 0;
+  };
+})());
+
+window.starStorage = (new (function(){
+  
+  var storage;    //声明一个变量，用于确定使用哪个本地存储函数
+
+  if(window.localStorage){
+      storage = localStorage;     //当localStorage存在，使用H5方式
+  }
+  else{
+      storage = cookieStorage;    //当localStorage不存在，使用兼容方式
+  }
+
+  this.setItem = function(key, value){
+      storage.setItem(key, value);
+  };
+
+  this.getItem = function(name){
+      return storage.getItem(name);
+  };
+
+  this.removeItem = function(key){
+      storage.removeItem(key);
+  };
+
+  this.clear = function(){
+      storage.clear();
+  };
+})());
--- blyx-h5/WebContent/js/bl/star.common.js
+++ blyx-h5/WebContent/js/bl/star.common.js
@@ -34,54 +34,74 @@ function ajax(options){
   });
 }
 
+var login_user_info_key = 'login_user_2';
 function getLoginInfo(){
-  if(!localStorage.login_user_2 || localStorage.login_user_2 == 'undefined') return null;
-  var user = JSON.parse(localStorage.login_user_2);
-  return user;
+  try{
+    var info = starStorage.getItem(login_user_info_key);
+    if(!info || info == 'undefined') return null;
+    var user = JSON.parse(info);
+    return user;
+  }catch(e){
+    return null;
+  }
 }
 
 function getDistributorLoginInfo(){
-  var tem = cookieStorage.getItem("login_distributor_user");
-  if(!tem) return null;
-  var user = JSON.parse(tem);
-  return user;
+  try{
+    var tem = cookieStorage.getItem("login_distributor_user");
+    if(!tem) return null;
+    var user = JSON.parse(tem);
+    return user;
+  }catch(e){
+    return null;
+  }
 }
 
 function getShopInfo(){
-  if(!localStorage.distributor) return null;
-  var distributor = JSON.parse(localStorage.distributor);
-  return distributor;
+  try{
+    var info = starStorage.getItem("distributor");
+    if(!info) return null;
+    var distributor = JSON.parse(info);
+    return distributor;
+  }catch(e){
+    return null;
+  }
 }
 
 function islogin(){
-  if(!localStorage.login_user_2) return false;
-  var user = JSON.parse(localStorage.login_user_2);
-  return user.memberId > 0;
+  try{
+    var info = starStorage.getItem(login_user_info_key);
+    if(!info) return false;
+    var user = JSON.parse(info);
+    return user.memberId > 0;
+  }catch(e){
+    return false;
+  }
 }
 
 function setLoginInfo(user){
-  localStorage.login_user_2 = JSON.stringify(user);
+  starStorage.setItem(login_user_info_key, JSON.stringify(user));
 }
 
 function setShopInfo(distributor){
-  localStorage.distributor = JSON.stringify(distributor);
+  starStorage.setItem('distributor',JSON.stringify(distributor));
 }
 
 function logout(){
-  localStorage.removeItem('login_user_2');
+  starStorage.removeItem(login_user_info_key);
 }
 
 function putLocalData(key, value){
-  localStorage[key] = value;
+  starStorage.setItem(key, value);
 }
 
 function getLocalData(key){
-  return localStorage[key];
+  return starStorage.getItem(key);
 }
 
 function delLocalData(key){
-  var val = localStorage[key];
-  localStorage.removeItem(key);
+  var val = starStorage.getItem(key);
+  starStorage.removeItem(key);
   return val;
 }
 
@@ -141,3 +161,111 @@ Date.prototype.format = function (fmt) {
   if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
   return fmt;
  }
+window.cookieStorage = (new (function(){
+  var maxage = 60*60*24*1000;
+  var path = '/';
+
+  var cookie = getCookie();
+
+  function getCookie(){
+      var cookie = {};
+      var all = document.cookie;
+      if(all === "")
+          return cookie;
+      var list = all.split("; ");
+      for(var i=0; i < list.length; i++){
+          var cookies = list[i];
+          var p = cookies.indexOf("=");
+          var name = cookies.substring(0,p);
+          var value = cookies.substring(p+1);
+          value = decodeURIComponent(value);
+          cookie[name] = value;
+      }
+      return cookie;
+  }
+
+  var keys = [];
+  for(var key in cookie)
+      keys.push(key);
+
+  this.length = keys.length;
+
+  this.key = function(n){
+      if(n<0 || n >= keys.length)
+          return null;
+      return keys[n];
+  };
+
+  this.setItem = function(key, value){
+      if(! (key in cookie)){
+          keys.push(key);
+          this.length++;
+      }
+
+      cookie[key] = value;
+      var cookies = key + "=" +encodeURIComponent(value);
+      if(maxage)
+          cookies += "; max-age=" + maxage;
+      if(path)
+          cookies += "; path=" + path;
+      cookies += "; domain=hnkbmd.com";
+      document.cookie = cookies;
+  };
+
+  this.getItem = function(name){
+      return cookie[name] || null;
+  };
+
+  this.removeItem = function(key){
+      if(!(key in cookie))
+          return;
+
+      delete cookie[key];
+
+      for(var i=0; i<keys.length; i++){
+          if(keys[i] === key){
+              keys.splice(i, 1);
+              break;
+          }
+      }
+      this.length--;
+
+      document.cookie = key + "=; max-age=0; domain=hnkbmd.com";
+  };
+
+  this.clear = function(){
+      for(var i=0; i<keys.length; i++)
+          document.cookie = keys[i] + "; max-age=0; domain=hnkbmd.com";
+      cookie = {};
+      keys = [];
+      this.length = 0;
+  };
+})());
+
+window.starStorage = (new (function(){
+  
+  var storage;    //声明一个变量，用于确定使用哪个本地存储函数
+
+  if(window.localStorage){
+      storage = localStorage;     //当localStorage存在，使用H5方式
+  }
+  else{
+      storage = cookieStorage;    //当localStorage不存在，使用兼容方式
+  }
+
+  this.setItem = function(key, value){
+      storage.setItem(key, value);
+  };
+
+  this.getItem = function(name){
+      return storage.getItem(name);
+  };
+
+  this.removeItem = function(key){
+      storage.removeItem(key);
+  };
+
+  this.clear = function(){
+      storage.clear();
+  };
+})());
