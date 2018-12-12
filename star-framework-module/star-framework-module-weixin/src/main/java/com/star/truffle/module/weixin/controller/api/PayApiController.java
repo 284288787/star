24a47372932a671f62fc1c5f6e2d2f9c8350ba5e
284288787 <284288787@qq.com>
2018-12-12 15:33:57 +0800

    12

--- star-framework-module/star-framework-module-weixin/src/main/java/com/star/truffle/module/weixin/controller/api/PayApiController.java
+++ star-framework-module/star-framework-module-weixin/src/main/java/com/star/truffle/module/weixin/controller/api/PayApiController.java
@@ -76,14 +76,8 @@ public class PayApiController {
 //  }
   
   @RequestMapping(value = "/callback", method = RequestMethod.POST)
-  @ApiOperation(value = "统一下单", notes = "返回订单号，金额等信息，使客户端完成支付", httpMethod = "POST", response = ApiResult.class)
-  @ApiImplicitParams({
-    @ApiImplicitParam(name = "memberId", value = "会员Id", dataType = "Long", required = true, paramType = "query"),
-    @ApiImplicitParam(name = "openId", value = "openid", dataType = "String", required = true, paramType = "query"),
-    @ApiImplicitParam(name = "orderId", value = "主键", dataType = "Long", required = true, paramType = "query")
-  })
   public String callback(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    log.info(".................微信支付返回:" );
+    log.info(".................微信支付返回:");
     String url = request.getRequestURL().toString();
     log.info(url);
     Enumeration<String> headers = request.getHeaderNames();
@@ -98,7 +92,7 @@ public class PayApiController {
     byte[] b = new byte[1024];
     int len = 0;
     while ((len = is.read(b)) != -1) {
-        sBuffer.append(new String(b, 0, len));
+      sBuffer.append(new String(b, 0, len));
     }
     String resXml = sBuffer.toString();
     resXml = resXml.replace("<xml>", "<com.star.truffle.module.weixin.domain.PayDetailInfo>").replace("</xml>", "</com.star.truffle.module.weixin.domain.PayDetailInfo>");
@@ -107,9 +101,44 @@ public class PayApiController {
     PayDetailInfo payOrder = (PayDetailInfo) xStreamForRequestPostData.fromXML(resXml);
     boolean bool = payService.payCallback(payOrder);
     if (bool) {
-        return "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>";
+      return "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>";
     } else {
-        return "<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[FAIL]]></return_msg></xml>";
+      return "<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[FAIL]]></return_msg></xml>";
     }
-}
+  }
+  
+  @RequestMapping(value = "/callback2", method = RequestMethod.GET)
+  public String callback2(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    String resXml = "<com.star.truffle.module.weixin.domain.PayDetailInfo><appid><![CDATA[wx8a05f2d3eb34111f]]></appid>\r\n" + 
+        "<attach><![CDATA[1626]]></attach>\r\n" + 
+        "<bank_type><![CDATA[CFT]]></bank_type>\r\n" + 
+        "<cash_fee><![CDATA[3954]]></cash_fee>\r\n" + 
+        "<coupon_count><![CDATA[1]]></coupon_count>\r\n" + 
+        "<coupon_fee>6</coupon_fee>\r\n" + 
+        "<coupon_fee_0><![CDATA[6]]></coupon_fee_0>\r\n" + 
+        "<coupon_id_0><![CDATA[2000000064794819197]]></coupon_id_0>\r\n" + 
+        "<fee_type><![CDATA[CNY]]></fee_type>\r\n" + 
+        "<is_subscribe><![CDATA[Y]]></is_subscribe>\r\n" + 
+        "<mch_id><![CDATA[1518623831]]></mch_id>\r\n" + 
+        "<nonce_str><![CDATA[7030a7227e934db987f316f9acdd646d]]></nonce_str>\r\n" + 
+        "<openid><![CDATA[ooQ_o1Sk1f_KjJRxVM_o2wo1Snlw]]></openid>\r\n" + 
+        "<out_trade_no><![CDATA[1b941a40d7a24cbeb3a282310ce31403]]></out_trade_no>\r\n" + 
+        "<result_code><![CDATA[SUCCESS]]></result_code>\r\n" + 
+        "<return_code><![CDATA[SUCCESS]]></return_code>\r\n" + 
+        "<sign><![CDATA[3F447EADA5FECCBC14837B92E88F4FB5]]></sign>\r\n" + 
+        "<time_end><![CDATA[20181212125628]]></time_end>\r\n" + 
+        "<total_fee>3960</total_fee>\r\n" + 
+        "<trade_type><![CDATA[JSAPI]]></trade_type>\r\n" + 
+        "<transaction_id><![CDATA[4200000208201812123486625781]]></transaction_id>\r\n" + 
+        "</com.star.truffle.module.weixin.domain.PayDetailInfo>";
+    log.info(resXml);
+    XStream xStreamForRequestPostData = new XStream(new DomDriver("UTF-8", new XmlFriendlyNameCoder("-_", "_")));
+    PayDetailInfo payOrder = (PayDetailInfo) xStreamForRequestPostData.fromXML(resXml);
+    boolean bool = payService.payCallback(payOrder);
+    if (bool) {
+      return "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>";
+    } else {
+      return "<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[FAIL]]></return_msg></xml>";
+    }
+  }
 }
--- star-framework-module/star-framework-module-weixin/src/main/java/com/star/truffle/module/weixin/domain/PayDetailInfo.java
+++ star-framework-module/star-framework-module-weixin/src/main/java/com/star/truffle/module/weixin/domain/PayDetailInfo.java
@@ -18,6 +18,7 @@ public class PayDetailInfo {
   private String mch_id;
   private String nonce_str;
   private String sign;
+  private String sign_type;   //1
   private String result_code; // SUCCESS/FAIL 
   private String err_code;
   private String err_code_des;
@@ -29,14 +30,21 @@ public class PayDetailInfo {
   private String is_subscribe;
   private String trade_type;
   private String bank_type;
-  private Long total_fee;
+  private Long total_fee;                  //订单总金额，单位为分
   private String coupon_fee;
   private Integer coupon_count;
-  private String fee_type;
+  private String coupon_type_0;         //CASH
+  private String coupon_type_1;         //NO_CASH
+  private String coupon_id_0; 
+  private String coupon_id_1; 
+  private Integer coupon_fee_0; 
+  private Integer coupon_fee_1; 
+  private Integer settlement_total_fee; //1  应结订单金额=订单金额-非充值代金券金额，应结订单金额<=订单金额。
+  private String fee_type;                  //货币类型，符合ISO4217标准的三位字母代码，默认人民币：CNY
   private String transaction_id;
   private String out_trade_no;
   private String attach;
   private String time_end;
-  private Long cash_fee;
+  private Long cash_fee;           //现金支付金额订单现金支付金额，详见支付金额
   private String cash_fee_type;
 }
