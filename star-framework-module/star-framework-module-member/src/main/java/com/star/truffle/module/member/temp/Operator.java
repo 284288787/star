56700a2d01f9a94bfaec0b3267c78cb9da0d0e20
284288787 <284288787@qq.com>
2019-03-25 11:46:50 +0800

    7

--- star-framework-module/star-framework-module-member/src/main/java/com/star/truffle/module/member/temp/Operator.java
+++
@@ -1,9 +0,0 @@
-/**create by liuhua at 2018年10月31日 上午10:12:10**/
-package com.star.truffle.module.member.temp;
-
-public class Operator {
-
-  private Long operatorId;
-  private Long distributorId;
-  private Long parentId;
-}
